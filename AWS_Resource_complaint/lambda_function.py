a0899c00acf1e4f0e4690f145329af64d0eea4d1
Mushfiqueghayas <mushfiqueshaikh69@gmail.com>
2026-07-02 23:21:48 +0530

    Enhance comments in lambda_function.py
    Added comments to clarify the Lambda function's behavior and the structure of the event object.

--- AWS_Resource_complaint/lambda_function.py
+++ AWS_Resource_complaint/lambda_function.py
@@ -10,6 +10,17 @@ def lambda_handler(event, context):
     compliance_status = "COMPLIANT"  
     
     # Extract the configuration item from the invokingEvent
+    #The event Object (What Triggers This Lambda)
+    #When AWS Config detects a change in your infrastructure, it calls this Lambda and sends an event dictionary. Here is what that event looks like:
+    #######
+    {
+      "invokingEvent": "{\"configurationItem\":{\"configurationItemCaptureTime\":\"2024-01-15T10:30:00Z\",\"configurationItemStatus\":\"OK\",\"resourceType\":\"AWS::EC2::Instance\",\"resourceId\":\"i-0abc123def456\",\"configuration\":{\"instanceId\":\"i-0abc123def456\",\"instanceType\":\"t3.medium\",\"state\":{\"name\":\"running\"}}},\"notificationCreationTime\":\"2024-01-15T10:30:05Z\",\"messageType\":\"ConfigurationItemChangeNotification\"}",
+
+      "resultToken": "myResultToken12345"
+    }
+    #######
+
+    #It convert the json string into dictionary
     config = json.loads(event['invokingEvent'])
     
     configuration_item = config["configurationItem"]
@@ -18,6 +29,20 @@ def lambda_handler(event, context):
     instance_id = configuration_item['configuration']['instanceId']
     
     # Get complete Instance details
+    # (InstanceIds=[instance_id])  ======  means filter only particular instanceId, means only one resevationId will be there.
+    ######
+    {
+      "Reservations": [
+        {
+          "ReservationId": "r-001",
+          "Instances": [
+            { "InstanceId": "i-aaa111", "InstanceType": "t3.medium" },
+            { "InstanceId": "i-bbb222", "InstanceType": "t3.medium" }
+          ]
+        {
+      [    
+    }
+    ######
     instance = ec2_client.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]
     
     # Check if the specific EC2 instance has Cloud Trail logging enabled.
@@ -25,6 +50,7 @@ def lambda_handler(event, context):
     if not instance['Monitoring']['State'] == "enabled":
         compliance_status = "NON_COMPLIANT"
 
+    #### This is the result you are sending back to AWS Config. It looks like:
     evaluation = {
         'ComplianceResourceType': 'AWS::EC2::Instance',
         'ComplianceResourceId': instance_id,
@@ -34,6 +60,8 @@ def lambda_handler(event, context):
     }
     
     config_client = boto3.client('config')
+
+    ### put_evaluations sends the compliance result to AWS Config.
     
     response = config_client.put_evaluations(
         Evaluations=[evaluation],
